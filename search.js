c02fb5fa96d1d0b906b41204ab4bceaf6c70009d
Lulush <czensulejman@gmail.com>
2023-01-24 23:28:15 +0300

    добавление новой книги, когда дочитали старую

--- search.js
+++ search.js
@@ -1,15 +1,16 @@
 import { BOOKS } from "./meta/booksIdMap.js";
 
-let bookId;
+let currentBookId;
 
 init();
 
+Object.entries(BOOKS);
 
 function init() {
     _search.onkeyup = (event) => showSearchResults(event.target.value);
     _search.onclick = (event) => showSearchResults(event.target.value);
     const book = BOOKS[Math.floor(Math.random() * BOOKS.length)];
-    bookId = book.id;
+    const bookId = book.id;
     setContent(bookId);
 }
 
@@ -23,7 +24,7 @@ export function showSearchResults(input) {
     const ul = document.createElement('ul');
     ul.id="search-result"
     ul.onclick = (event) => {
-        bookId = event.target.dataset.id;
+        const bookId = event.target.dataset.id;
         setContent(bookId);        
     }
     const items = `${suggestions.map((book) => `<li data-id="${book.id}">${book.name}</li>`).join('')}`;
@@ -35,6 +36,7 @@ export function showSearchResults(input) {
 }
 
 function setContent(bookId) {
+    currentBookId = bookId;
     const book = BOOKS.find((book) => book.id === bookId);
     if (book) {
         fetch(book.url)
@@ -42,6 +44,26 @@ function setContent(bookId) {
         .then((content) => {
             _content.innerHTML = content;
             _booksSuggestion.classList.add("hide");
+            window.scrollTo(0,0);
         });
     }
 }
+
+document.onscroll = () => {
+    let windowRelativeBottom = document.documentElement.getBoundingClientRect().bottom;
+    
+        // если пользователь прокрутил достаточно далеко (< 100px до конца)
+        if (windowRelativeBottom < document.documentElement.clientHeight + 100) {                       
+            let nextBookId;     // создаем переменную для айди следующей книги
+            BOOKS.find((    // перебираем наш список
+                item,  // текущий элемент в нашем списке
+                ind    // текущий индекс нашего элемента в списке         
+            ) => {
+                if (item.id === currentBookId) {  // сравниваем айди нашего элемента с текущим айди
+                    nextBookId = BOOKS[ind + 1] ? BOOKS[ind + 1].id : BOOKS[0].id; // записываем в переменную индекс следующего элемента
+                    return true; // возращяем ИСТИНА чтобы программа остановилась и не читала список дальше
+                }
+            });
+            setContent(nextBookId); // используем нашу новую переменную , которая содержит в себе следующую книгу
+        }    
+    };
--- utils.js
+++ utils.js
@@ -10,22 +10,5 @@
 //         };
 // }
 
-document.onscroll = () => {
-    let windowRelativeBottom = document.documentElement.getBoundingClientRect().bottom;
-    
-        // если пользователь прокрутил достаточно далеко (< 100px до конца)
-        if (windowRelativeBottom < document.documentElement.clientHeight + 100) {
-            console.log(123);
-            // nextBookId = это айди следующей книги
-            // BOOKS bookId текущей
 
-            // В спискеВсехКниг найти книгу с айдиТекущейКниги => текущаяКнига
-            // Перейти к следующей книге и обновить глобальныйАйди
-            // перейти к содержимому этой книги
-
-
-            // setContent(nextBookId);
-        }
-    
-    };
 
